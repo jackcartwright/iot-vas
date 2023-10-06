184b5dcf4a131e94fcf8874f5d8c7823d5d50f84
Jack Cartwright <contact@jackc.art>
2023-10-06 11:23:50 -0400

    Do not use flask-restul  - Update app.py to not use flask-restful  - Update requirements file to remove flask-restful

--- app/app.py
+++ app/app.py
@@ -1,7 +1,4 @@
-import sys
-
 from flask import Flask
-from flask_restful import Resource, Api
 
 from gvm.connections import UnixSocketConnection
 from gvm.errors import GvmError
@@ -11,7 +8,6 @@ from gvm.transforms import EtreeCheckCommandTransform
 from lxml import etree
 
 from werkzeug.middleware.proxy_fix import ProxyFix
-
 
 path = '/run/gvmd/gvmd.sock'
 connection = UnixSocketConnection(path=path)
@@ -30,55 +26,49 @@ app = Flask(__name__)
 app.wsgi_app = ProxyFix(
     app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_port=1, x_prefix=1
 )
-api = Api(app)
 
-class version(Resource):
-    def get(self):
+@app.route("/version")
+def get_version():
+    with Gmp(connection=connection, transform=transform) as gmp:
+        response = gmp.get_version()
+        vers = response.xpath('version/text()')[0]
+    return {'version': vers}
+
+@app.route("/scanners")
+def get_scanners():
+    try:
         with Gmp(connection=connection, transform=transform) as gmp:
-            response = gmp.get_version()
-            vers = response.xpath('version/text()')[0]
-        return {'version': vers}
+            gmp.authenticate(username, password)
+            response = gmp.get_scanners()
+            scanners = response.xpath('scanner/@id')
+            names = response.xpath('scanner/name/text()')
+        return {'scanners': scanners, 'names': names}
+    except GvmError as e:
+        return {'error': '1'}
 
-class scanners(Resource):
-    def get(self):
-        try:
-            with Gmp(connection=connection, transform=transform) as gmp:
-                gmp.authenticate(username, password)
-                response = gmp.get_scanners()
-                scanners = response.xpath('scanner/@id')
-                names = response.xpath('scanner/name/text()')
-            return {'scanners': scanners, 'names': names}
-        except GvmError as e:
-            return {'error': '1'}
+@app.route("/configs")
+def get_configs():
+    try:
+        with Gmp(connection=connection, transform=transform) as gmp:
+            gmp.authenticate(username, password)
+            response = gmp.get_scan_configs()
+            ids = response.xpath('config/@id')
+            names = response.xpath('config/name/text()')
+        return {'configs': ids, 'names': names}
+    except GvmError as e:
+        return {'error': '1'}
 
-class configs(Resource):
-    def get(self):
-        try:
-            with Gmp(connection=connection, transform=transform) as gmp:
-                gmp.authenticate(username, password)
-                response = gmp.get_scan_configs()
-                ids = response.xpath('config/@id')
-                names = response.xpath('config/name/text()')
-            return {'configs': ids, 'names': names}
-        except GvmError as e:
-            return {'error': '1'}
-
-class port_lists(Resource):
-    def get(self):
-        try:
-            with Gmp(connection=connection, transform=transform) as gmp:
-                gmp.authenticate(username, password)
-                response = gmp.get_port_lists()
-                ids = response.xpath('port_list/@id')
-                names = response.xpath('port_list/name/text()')
-            return {'port_lists': ids, 'names': names}
-        except GvmError as e:
-            return {'error': '1'}
-
-api.add_resource(version, '/version')
-api.add_resource(scanners, '/scanners')
-api.add_resource(configs, '/configs')
-api.add_resource(port_lists, '/port_lists')
+@app.route("/port_lists")
+def get_port_lists():
+    try:
+        with Gmp(connection=connection, transform=transform) as gmp:
+            gmp.authenticate(username, password)
+            response = gmp.get_port_lists()
+            ids = response.xpath('port_list/@id')
+            names = response.xpath('port_list/name/text()')
+        return {'port_lists': ids, 'names': names}
+    except GvmError as e:
+        return {'error': '1'}
 
 if __name__ == '__main__':
     app.run(debug=True)
