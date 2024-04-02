30cbeade70c525277cff8962ed75d85ab1afce31
Jack Cartwright <contact@jackc.art>
2024-04-02 15:23:40 -0400

    Update db.sql  - Add tasks table to support current task functionality  - Add reports table for future reports functionality

--- db/db.sql
+++ db/db.sql
@@ -10,3 +10,17 @@ CREATE TABLE targets (
     owner varchar REFERENCES users(name) NOT NULL,
     CONSTRAINT targets_name_owner UNIQUE (name, owner)
 );
+
+CREATE TABLE tasks (
+    uuid varchar PRIMARY KEY,
+    name varchar NOT NULL,
+    owner varchar REFERENCES users(name) NOT NULL,
+    target varchar REFERENCES targets(uuid) NOT NULL,
+    CONSTRAINT tasks_name_owner UNIQUE (name, owner)
+);
+
+CREATE TABLE reports (
+    uuid varchar PRIMARY KEY,
+    owner varchar REFERENCES users(name) NOT NULL,
+    task varchar REFERENCES tasks(uuid) NOT NULL
+);
